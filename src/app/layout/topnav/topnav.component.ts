85ab3cd2beba7515d60dba18f86692d5846f1080
Ryan McCormick <ryan@itjobsguy.com>
2017-12-17 18:32:29 -0600

    add inlist boilerplate

--- src/app/layout/topnav/topnav.component.ts
+++ src/app/layout/topnav/topnav.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopnavComponent implements OnInit {
 
+  title = 'SQL Tools';
+
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/sqlinlist/sqlinlist.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sqlinlist',
+  templateUrl: './sqlinlist.component.html',
+  styleUrls: ['./sqlinlist.component.scss']
+})
+export class SqlinlistComponent implements OnInit {
+
+  title = 'Make SQL IN-LIST';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
